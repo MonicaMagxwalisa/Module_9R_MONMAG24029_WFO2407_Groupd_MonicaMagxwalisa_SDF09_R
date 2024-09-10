ab33c853fcead034d15c26d20ac9c2e25868cab1
Monica Magxwalisa <monmagx@gmail.com>
2024-09-10 14:49:10 +0200

    update index.js

--- index.js
+++ index.js
@@ -66,4 +66,18 @@ function getRandomCard(){
     } else {
         return randomNumer
     }
+    
 }
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
